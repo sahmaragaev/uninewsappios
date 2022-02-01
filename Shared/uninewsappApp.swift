699537dc797a3a39c220c022b0ae624f2d6be0bd
sahmaragaev <sahmaraqayev32@gmail.com>
2022-01-31 21:57:06 -0800

    first commit

---
+++ Shared/uninewsappApp.swift
@@ -0,0 +1,17 @@
+//
+//  uninewsappApp.swift
+//  Shared
+//
+//  Created by Sahmar Agaev on 31.01.22.
+//
+
+import SwiftUI
+
+@main
+struct uninewsappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
